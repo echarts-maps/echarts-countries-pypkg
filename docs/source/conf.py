78f0848291ecaf585f4d4e0029a54e14559773e2
chfw <wangc_2011@hotmail.com>
2018-02-27 00:47:12 +0000

    :egg: release 0.1.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,8 @@ master_doc = 'index'
 
 project = u'echarts-countries-pypkg'
 copyright = u'2018 pyecharts dev team'
-version = '0.1.2'
-release = '0.1.2'
+version = '0.1.3'
+release = '0.1.3'
 exclude_patterns = []
 pygments_style = 'sphinx'
 html_theme = 'default'
